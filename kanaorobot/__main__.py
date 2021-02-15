43d291c4a2c987093b68be0fe35ed4df22ba4a58
animespotflix <79033810+animespotflix@users.noreply.github.com>
2021-02-15 14:43:18 +0530

    Create __main__.py

---
+++ kanaorobot/__main__.py
@@ -0,0 +1,6 @@
+from kanaorobot import KANAO
+from kanaorobot.modules import start, help, anilist, schedule, nhentai, inline, callback, whatanime 
+from pyrogram import filters
+
+if __name__ == "__main__":
+    KANAO.run()
